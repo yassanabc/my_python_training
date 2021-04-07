27f995dde65c991eedd98ae4331f71deacf71eb9
yassan <yassan.bird.1305@icloud.com>
2021-04-07 23:44:54 +0000

    update

--- Roboter/roboter.py
+++ Roboter/roboter.py
@@ -8,16 +8,29 @@ restaurants_dict = dict()
 with open('./test/Roboter/store.json', mode='rt', encoding='utf-8') as file:
     restaurants_dict = json.load(file)
 logging.debug('restaurants: %s',restaurants_dict)
+logging.debug('restaurants type: %s',type(restaurants_dict))
 
 print('こんにちは！私はRobokoです。あなたの名前はなんですか？')
-name = input()
+user_name = input()
 
 if restaurants_dict == None:
-    print('%sさん。どこのレストランが好きですか？')
+    print('{0}さん。どこのレストランが好きですか？'.format(user_name))
     restaurants_dict = input()
 else:
-    pass
+    # ソート
+    restaurants_dict = sorted(restaurants_dict.items(), key=lambda x:x[0])
+    print("私のおすすめのレストランは、{0}です。\nこのレストランは好きですか？[Yes][No]".format(restaurants_dict[0][0]))
+    like = input()
+    if 'Y' in like or 'y' in like:
+        restaurants_dict[0][1] += 1
+    else:
+        print('{0}さん。どこのレストランが好きですか？'.format(user_name))
+        restaurant_name = input()
 
+output_dict = dict(restaurants_dict)
+print(output_dict)
+
+print('{0}さん、ありがとうございました。\n良い1日を！さようなら。'.format(user_name))
 # ソート
 # 標準入出力
 # 結果をjsonファイルに保存
